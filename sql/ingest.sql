0e78338d0157d76e7ef8263c79873267d135d82a
jeriraooo <jerirao@foxmail.com>
2023-11-01 20:00:32 -0400

    add restriction to ingest data
    revised the way to ingest data and updated row count

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -3,14 +3,14 @@
 -- Turn on echo to display executed commands
 .echo on
 
--- Importing CSV files
-CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/central_park_weather.csv');
-CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/citibike-tripdata.csv');
-CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/fhv_bases.csv');
-
+-- Importing CSV files with the correct parameters
+-- Assuming all CSV files have headers and should treat all columns as VARCHAR
+CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/central_park_weather.csv', HEADER=TRUE, ALL_VARCHAR=TRUE);
+CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/citibike-tripdata.csv', HEADER=TRUE, ALL_VARCHAR=TRUE);
+CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('~/Downloads/project-data/data/fhv_bases.csv', HEADER=TRUE, ALL_VARCHAR=TRUE);
 
 -- Importing Parquet files
-CREATE TABLE fhv_tripdata AS SELECT * FROM parquet_scan('~/Downloads/project-data/data/taxi/fhv_tripdata.parquet');
-CREATE TABLE fhvhv_tripdata AS SELECT * FROM parquet_scan('~/Downloads/project-data/data/taxi/fhvhv_tripdata.parquet');
-CREATE TABLE green_tripdata AS SELECT * FROM parquet_scan('~/Downloads/project-data/data/taxi/green_tripdata.parquet');
-CREATE TABLE yellow_tripdata AS SELECT * FROM parquet_scan('~/Downloads/project-data/data/taxi/yellow_tripdata.parquet');
+CREATE TABLE fhv_tripdata AS SELECT *, 'fhv_tripdata.parquet' as filename FROM parquet_scan('~/Downloads/project-data/data/taxi/fhv_tripdata.parquet');
+CREATE TABLE fhvhv_tripdata AS SELECT *, 'fhvhv_tripdata.parquet' as filename FROM parquet_scan('~/Downloads/project-data/data/taxi/fhvhv_tripdata.parquet');
+CREATE TABLE green_tripdata AS SELECT *, 'green_tripdata.parquet' as filename FROM parquet_scan('~/Downloads/project-data/data/taxi/green_tripdata.parquet');
+CREATE TABLE yellow_tripdata AS SELECT *, 'yellow_tripdata.parquet' as filename FROM parquet_scan('~/Downloads/project-data/data/taxi/yellow_tripdata.parquet');
